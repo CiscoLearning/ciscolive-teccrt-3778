9a8cf5fd39420cd46bb21096658feaa4c52880c2
Hank Preston <hank.preston@gmail.com>
2022-06-07 01:58:12 +0000

    initial python code

--- casestudy-nso-01/acl-service/python/acl_service/main.py
+++ casestudy-nso-01/acl-service/python/acl_service/main.py
@@ -3,42 +3,33 @@ import ncs
 from ncs.application import Service
 
 
-# ------------------------
-# SERVICE CALLBACK EXAMPLE
-# ------------------------
 class ServiceCallbacks(Service):
 
-    # The create() callback is invoked inside NCS FASTMAP and
-    # must always exist.
     @Service.create
     def cb_create(self, tctx, root, service, proplist):
         self.log.info('Service create(service=', service._path, ')')
 
         vars = ncs.template.Variables()
-        vars.add('DUMMY', '127.0.0.1')
         template = ncs.template.Template(service)
-        template.apply('acl-service-template', vars)
 
-    # The pre_modification() and post_modification() callbacks are optional,
-    # and are invoked outside FASTMAP. pre_modification() is invoked before
-    # create, update, or delete of the service, as indicated by the enum
-    # ncs_service_operation op parameter. Conversely
-    # post_modification() is invoked after create, update, or delete
-    # of the service. These functions can be useful e.g. for
-    # allocations that should be stored and existing also when the
-    # service instance is removed.
+        # ACL_NAME, ACTION, DESCIPTION, DESTINATION_ADDRESS, DESTIONATION_PORT, DEVICE_NAME, PROTOCOL, SOURCE_ADDRESS, SOURCE_PORT
+        vars.add("ACL_NAME", service.name)
 
-    # @Service.pre_lock_create
-    # def cb_pre_lock_create(self, tctx, root, service, proplist):
-    #     self.log.info('Service plcreate(service=', service._path, ')')
+        # Loop over rules 
+        for i, rule in enumerate(service.rule): 
+            vars.add("DESCRIPTION", f"Rule {i} in ACL Service {service.name}. Description {rule.description}")
+            vars.add("ACTION", rule.action)
+            vars.add("PROTOCOL", rule.protocol)
+            vars.add("SOURCE_ADDRESS", "any")
+            vars.add("SOURCE_PORT", "")
+            vars.add("DESTINATION_ADDRESS", "192.168.10.0 0.0.0.255")
+            vars.add("DESTINATION_PORT", "eq 80")
+            vars.add("LOG", rule.log)
 
-    # @Service.pre_modification
-    # def cb_pre_modification(self, tctx, op, kp, root, proplist):
-    #     self.log.info('Service premod(service=', kp, ')')
-
-    # @Service.post_modification
-    # def cb_post_modification(self, tctx, op, kp, root, proplist):
-    #     self.log.info('Service postmod(service=', kp, ')')
+            # loop over devices to apply 
+            for device in service.device: 
+                vars.add("DEVICE_NAME", device)
+                template.apply('acl-service-template', vars)
 
 
 # ---------------------------------------------
@@ -54,12 +45,6 @@ class Main(ncs.application.Application):
         # as specified in the corresponding data model.
         #
         self.register_service('acl-service-servicepoint', ServiceCallbacks)
-
-        # If we registered any callback(s) above, the Application class
-        # took care of creating a daemon (related to the service/action point).
-
-        # When this setup method is finished, all registrations are
-        # considered done and the application is 'started'.
 
     def teardown(self):
         # When the application is finished (which would happen if NCS went
